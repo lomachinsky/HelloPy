c3d58c5ee4102b9030be49850b6ef4a784e08ca2
XEON <v.lomachinsky@gmail.com>
2023-02-25 10:57:46 +0200

    Fix005

--- modules/api_interaction.py
+++ modules/api_interaction.py
@@ -2,16 +2,27 @@ import http.client
 import json
 
 
+def get_ini():
+
+    with open('ini.json') as f:
+        file_content = f.read()
+        templates = json.loads(file_content)
+
+    return templates
+
+
 def get_supp_ticket():
 
-    conn = http.client.HTTPSConnection("portal.ctrs.com.ua")
+    ini_content = get_ini().get('supp')
+
+    conn = http.client.HTTPSConnection(ini_content.get('url'))
     payload = json.dumps({
         "getTicket": 1000
     })
     headers = {
-        'Authorization': 'Bearer rkv2TSLP71ac9295bb300468d3880aa9f1a09736',
+        'Authorization': 'Bearer '+ini_content.get('bearer'),
         'Content-Type': 'application/json',
-        'Cookie': 'BITRIX_SM_GUEST_ID=427533; BITRIX_SM_LAST_VISIT=23.02.2023%2017%3A58%3A37; PHPSESSID=GwkQDoIPyq6TxieIvI1e5IHRX7VTXw2K'
+        'Cookie': ini_content.get('cookie')
     }
     conn.request("GET", "/api/supp/", payload, headers)
     res = conn.getresponse()
