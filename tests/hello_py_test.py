dca68d58ded60054e46c94899b93222e781c021d
lomachinsky <87434130+lomachinsky@users.noreply.github.com>
2023-03-01 23:11:31 +0200

    Update hello_py_test.py

--- tests/hello_py_test.py
+++ tests/hello_py_test.py
@@ -1,6 +1,3 @@
-import pytest
-import modules.api_interaction as api_int
-
 
 def test_get_supp_ticket():
     assert '{"success":true,"data":[],"errors":[]}' == '{"success":true,"data":[],"errors":[]}'
