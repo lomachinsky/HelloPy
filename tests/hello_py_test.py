d446dbfa64d8c42afb96efa98a47b32a8e3b615e
lomachinsky <87434130+lomachinsky@users.noreply.github.com>
2023-03-01 23:13:06 +0200

    Update hello_py_test.py

--- tests/hello_py_test.py
+++ tests/hello_py_test.py
@@ -1,7 +1,2 @@
-
-def test_get_supp_ticket():
-    assert '{"success":true,"data":[],"errors":[]}' == '{"success":true,"data":[],"errors":[]}'
-
-
-if __name__ == '__main__':
-    pytest.main()
+def test1():
+    assert 1 == 1
